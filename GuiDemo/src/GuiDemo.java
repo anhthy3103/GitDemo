d2893d356439af3b8d7017748e86dbe7220e98b9
Thy <anhthy3103@gmail.com>
2016-11-02 17:07:57 +0700

    Init project

---
+++ GuiDemo/src/GuiDemo.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class GuiDemo extends JFrame {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GuiDemo f = new GuiDemo();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
